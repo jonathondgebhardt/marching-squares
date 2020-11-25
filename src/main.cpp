7a2bfa9deaa761ac12b49229f08b11d436b6c89b
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2020-11-24 20:41:43 -0500

    Parse file more safely

--- src/main.cpp
+++ src/main.cpp
@@ -10,61 +10,21 @@
 struct Record
 {
   bool isValid() const { return this->elevations.size() == this->numElevations; }
-  void dump() const
+  void dump(bool verbose = false) const
   {
     std::cout << "numElevations: " << this->numElevations << std::endl;
     std::cout << "elevations.size(): " << this->elevations.size() << std::endl;
-    // std::copy(this->elevations.begin(), this->elevations.end(),
-    //           std::ostream_iterator<std::string>(std::cout, "\n"));
 
-    // for (size_t i = 0; i < this->elevations.size(); ++i)
-    // {
-    //   std::cout << "\t[" << i << "]: " << this->elevations[i] << std::endl;
-    // }
+    if (verbose)
+    {
+      std::copy(this->elevations.begin(), this->elevations.end(),
+                std::ostream_iterator<double>(std::cout, "\n"));
+    }
   }
 
   std::vector<double> elevations;
   size_t numElevations;
 };
-
-std::vector<double> extractElevations(const std::vector<std::string> &x, size_t offset = 0)
-{
-  std::vector<double> elevations;
-
-  for (size_t i = offset; i < x.size(); ++i)
-  {
-    try
-    {
-      elevations.push_back(std::stof(x[i]));
-    }
-    catch (const std::exception &e)
-    {
-      std::cerr << e.what() << ": could not interpret elevation" << std::endl;
-    }
-  }
-
-  return elevations;
-}
-
-Record makeRecord(const std::vector<std::string> &x)
-{
-  Record r;
-
-  try
-  {
-    r.numElevations = std::stoi(x[2]);
-  }
-  catch (const std::exception &e)
-  {
-    std::cerr << e.what() << ": could not interpret number of elevations" << std::endl;
-  }
-
-  constexpr size_t beginElevationIndex = 9;
-  const auto elevations = extractElevations(x, beginElevationIndex);
-  std::copy(elevations.begin(), elevations.end(), std::back_inserter(r.elevations));
-
-  return r;
-}
 
 std::vector<Record> makeRecords(const std::vector<std::string> &x)
 {
@@ -73,37 +33,50 @@ std::vector<Record> makeRecords(const std::vector<std::string> &x)
   auto it = x.begin();
   while (it != x.end())
   {
+    Record r;
+
+    std::optional<double> numElevationsOpt;
+
     try
     {
-      Record r;
-
       constexpr auto numElevationsOffset = 2;
-      auto numElevationsIt = it + numElevationsOffset;
-      const auto &numElevationsStr = *numElevationsIt;
-      const auto numElevations = std::stoi(numElevationsStr);
-
-      r.numElevations = numElevations;
-
-      std::vector<double> elevations;
-
-      constexpr auto elevationsOffset = 9;
-      auto beginIt = it + elevationsOffset;
-      auto endIt = beginIt + numElevations;
-      std::transform(beginIt, endIt, std::back_inserter(elevations),
-                     [](const auto &x) { return std::stoi(x); });
-
-      r.elevations = elevations;
-      //   r.dump();
-
-      records.push_back(r);
-
-      it = endIt + 1;
-
-      std::cout << "reiterating at " << std::distance(x.begin(), it) << std::endl;
+      numElevationsOpt = std::stoi(*(it + numElevationsOffset));
     }
     catch (const std::exception &e)
     {
       std::cerr << e.what() << '\n';
+      numElevationsOpt = std::nullopt;
+    }
+
+    if (numElevationsOpt.has_value())
+    {
+      r.numElevations = numElevationsOpt.value();
+
+      constexpr auto elevationsOffset = 9;
+      const auto beginIt = it + elevationsOffset;
+      const auto endIt = beginIt + r.numElevations;
+
+      std::vector<double> elevations;
+
+      try
+      {
+        std::transform(beginIt, endIt, std::back_inserter(elevations),
+                       [](const auto &x) { return std::stoi(x); });
+
+        r.elevations = elevations;
+      }
+      catch (const std::exception &e)
+      {
+        std::cerr << e.what() << '\n';
+        r.numElevations = 0;
+        r.elevations.clear();
+      }
+
+      assert(r.isValid());
+
+      records.push_back(r);
+
+      it = endIt;
     }
   }
 
